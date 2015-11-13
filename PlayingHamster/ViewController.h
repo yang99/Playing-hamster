80af4c92ca1ab080f8f1af936534be8ba69afa4f
yang99 <996212017@qq.com>
2015-11-13 17:14:02 +0800

    Initial Commit

---
+++ PlayingHamster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayingHamster
+//
+//  Created by yangyao on 15/11/13.
+//  Copyright © 2015年 Yao Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
